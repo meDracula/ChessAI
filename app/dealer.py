8b295d9f7ae4492f7ce6b04be44055f665e2a954
meDracula <me.dracula@protonmail.com>
2022-04-13 12:03:04 +0200

    Pep8 conformity and forgotten self call to function

--- app/dealer.py
+++ app/dealer.py
@@ -1,5 +1,6 @@
 from random import shuffle
 
+
 class Dealer:
     def __init__(self):
         self.deck = self.init_deck()
@@ -8,13 +9,12 @@ class Dealer:
         return [self.deck.pop(0) for _ in range(n_cards)]
 
     def reset_deck(self):
-        self.deck = init_deck()
+        self.deck = self.init_deck()
 
     @classmethod
     def init_deck(cls):
-        suits = ["D", "H", "S","C"]  # D: DIAMOND, H: HEART, S: SPADE, C: CLUB
-        ranks = ["2", "3", "4", "5","6", "7", "8", "9", "10", "J", "D", "K", "A"]
+        suits = ["D", "H", "S", "C"] # D: DIAMOND, H: HEART, S: SPADE, C: CLUB
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A"]
         deck = [rank+suit for rank in ranks for suit in suits]
         shuffle(deck)
         return deck
-
