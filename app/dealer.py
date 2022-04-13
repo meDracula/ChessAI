2bd7a313dc20a49c7fb1c79724835b5ff014e042
meDracula <me.dracula@protonmail.com>
2022-04-13 11:53:08 +0200

    Create a card dealer
    Operations:
    - Create a new deck and shuffle it
    - Deal n cards
    - Reset deck
    
    This is from issue 11

---
+++ app/dealer.py
@@ -0,0 +1,20 @@
+from random import shuffle
+
+class Dealer:
+    def __init__(self):
+        self.deck = self.init_deck()
+
+    def deal(self, n_cards):
+        return [self.deck.pop(0) for _ in range(n_cards)]
+
+    def reset_deck(self):
+        self.deck = init_deck()
+
+    @classmethod
+    def init_deck(cls):
+        suits = ["D", "H", "S","C"]  # D: DIAMOND, H: HEART, S: SPADE, C: CLUB
+        ranks = ["2", "3", "4", "5","6", "7", "8", "9", "10", "J", "D", "K", "A"]
+        deck = [rank+suit for rank in ranks for suit in suits]
+        shuffle(deck)
+        return deck
+
