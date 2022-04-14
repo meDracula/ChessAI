34165d699c31a65f64780cf811378d397bd5c9e0
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2022-04-14 18:59:01 +0200

    - Added new class Scorer - Changed suits to lowercase in Dealer to match treys library

--- app/dealer.py
+++ app/dealer.py
@@ -13,7 +13,7 @@ class Dealer:
 
     @classmethod
     def init_deck(cls):
-        suits = ["D", "H", "S", "C"] # D: DIAMOND, H: HEART, S: SPADE, C: CLUB
+        suits = ["d", "h", "s", "c"] # d: DIAMOND, h: HEART, s: SPADE, c: CLUB
         ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A"]
         deck = [rank+suit for rank in ranks for suit in suits]
         shuffle(deck)
