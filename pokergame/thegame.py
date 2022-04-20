5711108f0550427e48c1ff4aa05edca44a64fa89
Markus Antonsen <markus-antonsen@hotmail.com>
2022-04-20 14:15:19 +0200

    Made the code better and made a settings folder.

---
+++ pokergame/thegame.py
@@ -0,0 +1,68 @@
+import sys
+
+import pygame
+from pokergame import settings
+
+
+class Game:
+    def __init__(self):
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
+        pygame.display.set_caption(settings.TITLE)
+        self.clock = pygame.time.Clock()
+        self.load_data()
+
+    def load_data(self):
+        pass
+
+    def new(self):
+        pass
+
+    def run(self):
+        # Game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(settings.FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+
+    def update(self):
+        # Update portion of the game loop
+        pass
+
+    def draw(self):
+        self.screen.fill(settings.BGCOLOR)
+        pygame.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.quit()
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+
+def main():
+    # Create the game Instance
+    g = Game()
+    g.show_start_screen()
+    while True:
+        g.new()
+        g.run()
+        g.show_go_screen()
+
+
+if __name__ == '__main__':
+    main()
