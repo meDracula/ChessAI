1e7aff4bd6c51a61dab0eed33f32ac3122a9e338
meDracula <me.dracula@protonmail.com>
2022-05-26 10:22:21 +0200

    Add dir file of location to data onto class PokerAI

--- neural_network/model.py
+++ neural_network/model.py
@@ -3,13 +3,15 @@ from neural_network.n_network import NeuralNetwork
 import torch
 
 class PokerAI:
+    dir_path = "/training/data/"
+
     def __init__(self, model):
         self.model = model
 
     @classmethod
     def load_model(cls, filename="dummy.ph"):
         full_path = path.abspath(__file__)
-        file_path = path.dirname(full_path) + "/training/data/" + filename
+        file_path = path.dirname(full_path) + cls.dir_path + filename
         model = NeuralNetwork(11, 64)
         model.load_state_dict(torch.load(file_path))
         model.eval()
